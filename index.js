681363806e95873e578e223c525ce915add1698d
Richie Bendall <richiebendall@gmail.com>
2019-12-02 16:17:46 +1300

    feat: Swap got for ky and support NodeJS v8.3+
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 "use strict"
 
-const got = require("got")
+const ky = require("ky-universal")
 const { load: $ } = require("cheerio")
 const { fromPromise } = require("universalify")
 const _ = require("lodash")
-const toBluebird = require("to-bluebird")
 const dayjs = require("dayjs")
+const NamedRegExp = require("named-regexp-groups")
 dayjs.extend(require("dayjs/plugin/customParseFormat"))
 
-module.exports = fromPromise(() => toBluebird(got("https://thebulletin.org/doomsday-clock/past-statements/").then(({ body }) => {
-    const raw = $(body)(".uabb-infobox-title").first().text()
-    const parsed = raw.match(/IT IS(?: STILL)? (?<mins>\d+)(?: AND A (?<half>HALF))? MINUTES TO MIDNIGHT/)
-    const mins = _.toNumber(parsed.groups.mins) + (parsed.groups.half ? 0.5 : 0)
-    return {
-        minutes: mins,
-        source: _.upperFirst(_.lowerCase(raw)),
-        time: dayjs("0 AM", "HH A").subtract(mins, "minute").format("HH:mm:ss A"),
-    }
-})))
+module.exports = fromPromise(() => ky("https://thebulletin.org/doomsday-clock/past-statements/")
+    .then((res) => res.text())
+    .then((body) => {
+        const raw = $(body)(".uabb-infobox-title").first().text()
+        const { groups: parsed } = raw.match(new NamedRegExp("IT IS(?: STILL)? (?<mins>\\d+)(?: AND A (?<half>HALF))? MINUTES TO MIDNIGHT"))
+        const mins = _.toNumber(parsed.mins) + (parsed.half ? 0.5 : 0)
+        return {
+            minutes: mins,
+            source: _.upperFirst(_.lowerCase(raw)),
+            time: dayjs("0 AM", "HH A").subtract(mins, "minute").format("HH:mm:ss A"),
+        }
+    }))
